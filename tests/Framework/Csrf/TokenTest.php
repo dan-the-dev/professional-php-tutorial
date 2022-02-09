c8489031f05b1e92d034733f0da4898b9924837b
dan-the-dev <daniele.scillia@gmail.com>
2022-02-09 11:42:39 +0100

    test: Added test for not equals tokens to Token class

--- tests/Framework/Csrf/TokenTest.php
+++ tests/Framework/Csrf/TokenTest.php
@@ -26,4 +26,11 @@ final class TokenTest extends TestCase
         $token2 = new Token('test');
         $this->assertTrue($token1->equals($token2));
     }
+
+    public function testTokenWithDifferentValueAreNotEquals(): void
+    {
+        $token1 = new Token('test1');
+        $token2 = new Token('test2');
+        $this->assertFalse($token1->equals($token2));
+    }
 }
