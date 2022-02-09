5de166153e0819d05a616f9e782a6f3c50eaee62
dan-the-dev <daniele.scillia@gmail.com>
2022-02-09 12:27:21 +0100

    test: added tests for StoredTokenValidator

---
+++ tests/Framework/Csrf/StoredTokenValidatorTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Framework\Csrf;
+
+use PHPUnit\Framework\TestCase;
+use SocialNews\Framework\Csrf\StoredTokenValidator;
+use SocialNews\Framework\Csrf\Token;
+use SocialNews\Framework\Csrf\TokenStorage;
+
+final class StoredTokenValidatorTest extends TestCase
+{
+    public function testValidatesTwoEqualsToken(): void
+    {
+        $tokenStorage = $this->createMock(TokenStorage::class);
+        $storedTokenValidator = new StoredTokenValidator($tokenStorage);
+
+        $tokenStorage->expects($this->once())
+            ->method('retrieve')
+            ->with('test')
+            ->willReturn(new Token('test-token'));
+
+        $actualResult = $storedTokenValidator->validate('test', new Token('test-token'));
+        $this->assertTrue($actualResult);
+    }
+
+    public function testItDoesntValidatesTwoDifferentToken(): void
+    {
+        $tokenStorage = $this->createMock(TokenStorage::class);
+        $storedTokenValidator = new StoredTokenValidator($tokenStorage);
+
+        $tokenStorage->expects($this->once())
+            ->method('retrieve')
+            ->with('test')
+            ->willReturn(new Token('test-token'));
+
+        $actualResult = $storedTokenValidator->validate('test', new Token('test-token-2'));
+        $this->assertFalse($actualResult);
+    }
+
+    public function testItDoesntValidateTokenWhenKeyDoesntExists(): void
+    {
+        $tokenStorage = $this->createMock(TokenStorage::class);
+        $storedTokenValidator = new StoredTokenValidator($tokenStorage);
+
+        $tokenStorage->expects($this->once())
+            ->method('retrieve')
+            ->with('test')
+            ->willReturn(null);
+
+        $actualResult = $storedTokenValidator->validate('test', new Token('test-token'));
+        $this->assertFalse($actualResult);
+    }
+}
